ef7a1abf59582671767864472c9d8a2c7d6cec6a
Aravind <aravindbujji7@gmail.com>
2024-02-17 17:05:03 +0530

    added implementation using load balancer

--- src/main/java/com/tel/employeeservice/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/tel/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,12 @@
 package com.tel.employeeservice.service.impl;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,14 +28,48 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Autowired
+	DiscoveryClient discoveryClient;
+	
+//	@Autowired
+//	LoadBalancerClient loadBalancerClient;
+//	
 	@Override
 	public EmployeeDTO getEmployee(int id) {
-		
-		AddressDTO addressDTO = restTemplate.postForObject("http://localhost:8081/address-service/api/get-address/{id}", null, AddressDTO.class, id);
+		AddressDTO addressDTO = restCallWithLoadBalancedAnnotation(id);
 		Employee employee = employeeRepo.findById(id).get();
 		EmployeeDTO employeeDTO = modelMapper.map(employee, EmployeeDTO.class);
 		employeeDTO.setAddressDTO(addressDTO);
 		return employeeDTO;
+	}
+
+	private AddressDTO restCallWithRestTemplet(int id) {
+		return restTemplate.postForObject("http://localhost:8081/address-service/api/get-address/{id}", null, AddressDTO.class, id);
+	}
+	
+	private AddressDTO restCallWithDiscoveryRestTemplet(int id) {
+		
+		List<ServiceInstance> instances = discoveryClient.getInstances("address-service");
+		ServiceInstance serviceInstance = instances.get(0);
+		String uri = serviceInstance.getUri().toString();
+		String contextPath = serviceInstance.getMetadata().get("configPath");
+		System.out.println(uri+contextPath);
+		AddressDTO addressDTO = restTemplate.postForObject(uri+contextPath+"/get-address/{id}", null, AddressDTO.class, id);
+		return addressDTO;
+	}
+	
+//	private AddressDTO restCallWithLoadBalancedWay(int id) {
+//		ServiceInstance serviceInstance = loadBalancerClient.choose("address-service");
+//		String url = serviceInstance.getUri().toString();
+//		String context = serviceInstance.getMetadata().get("configPath");
+//		AddressDTO addressDTO = restTemplate.postForObject(url+context+"/get-address/{id}", null, AddressDTO.class, id);
+//		return addressDTO;
+//	}
+	
+	private AddressDTO restCallWithLoadBalancedAnnotation(int id) {
+		
+		AddressDTO addressDTO = restTemplate.postForObject("http://address-service/address-service/api/get-address/{id}", null, AddressDTO.class, id);
+		return addressDTO;
 	}
 
 }
